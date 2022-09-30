e445f6092c0d36eae7a78cff676409b1901c6729
Fabian <fabian.argast@htldornbirn.at>
2022-09-30 14:34:01 +0200

    Fertiger Code

---
+++ src/Zufallszahl2.java
@@ -0,0 +1,36 @@
+
+import java.util.Random;
+
+public class Zufallszahl2 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int randomNumber = random.nextInt(100);
+
+        int randomNumber2 = random.nextInt(100);
+
+        if(randomNumber < randomNumber2 && randomNumber < 50) {
+            System.out.println("Zahl 1 ist kleiner als Zahl 2 und Mini");
+        }
+        if(randomNumber < 30 || randomNumber < 30){
+                System.out.println("Eine der Beiden ist kleiner als 30");
+            }
+            if(randomNumber < 50 && randomNumber2 != 50){
+                    System.out.println("Erste Zahl klein, zweite kein 50iger");
+                }
+
+        }
+
+        // Wenn die erste Zahl kleiner ist als die zweite UND die erste Zahl kleiner ist als 50
+        // dann gib aus "Zahl 1 ist kleiner als Zahl 2 und Mini"
+
+        // Wenn die erste Zahl kleiner ist als 30 oder die zweite Zahl kleiner ist als 30
+        // dann gib aus "Eine der beiden ist kleiner als 30"
+
+        // Wenn die erste Zahl kleiner ist als 50 UND die zweite Zahl ungleich 50 ist
+        // dann gib aus "Erste Zahl klein, zweite kein 50iger"
+
+
+
+    }
+
+
